651053e643d353569ea8a3be717e6fda444c50a1
Avik Kuthiala <avikkuthiala@gmail.com>
2025-07-18 20:42:17 -0400

    add port support

--- runmodel.sh
+++ runmodel.sh
@@ -5,15 +5,19 @@
 # python3 -m accelerate.commands.launch \
 #         --num_processes=1 \
 
-export MODEL="gpt4v"
-export TASK="vid_sub"
-export OUTDIR="logs/logs_$MODEL"
-torchrun --nproc_per_node=1 -m lmms_eval \
+# export OUTDIR="logs/logs_$MODEL"
+export MODEL="ola"
+export TASK="audio_only"
+export OUTDIR="logs/logs_ola"
+CUDA_VISIBLE_DEVICES=5 torchrun --rdzv_backend=c10d --rdzv_endpoint=localhost:29535 --nproc_per_node=1 -m lmms_eval \
         --model $MODEL \
         --tasks maverix_$TASK \
         --batch_size 1 \
         --log_samples \
         --log_samples_suffix maverix_${MODEL}_${TASK} \
         --output_path ./$OUTDIR/${TASK}/ \
+        # --log_samples_suffix maverix_gpt4omini_${TASK} \
         # --limit 20 \
         # --verbosity=DEBUG
+
+#to do debug ola audio only. all gpt4o and mini done
